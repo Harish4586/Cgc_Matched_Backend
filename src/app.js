5b6441f584a22444351720a722a91ca4306a2052
Harish <nirbaanharish@gmail.com>
2025-01-21 07:05:25 +0530

    get post delete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const app = express();
 // console.log("new project");
 
-app.get((req,res)=>{
-    res.send("namaste from homepage");
-});
-app.get("/login",(req,res)=>{
-    res.send("hello from login");
-});
-app.get("/test",(req,res)=>{
-    res.send("hello from test");
+//this will listen to all http requests whether they are get or post etc.....
+
+// app.use("/",(req,res)=>{
+//     res.send("namaste from homepage");
+// });
+
+// but the app.get will only listen to http get request
+app.get("/", (req, res) => {
+  res.send("namaste from homepage");
 });
 
-app.listen(1000,()=>console.log("listening successfully on port number 1000 "));
+// app.use("/user",(req,res)=>{ 
+//         res.send("namaste from homepage");
+// });    
+
+//our router will not listen to any http request if we use {app.use("/user"),()=>{}} it here rather then
+//we'll use it after all the get ,post,patch ,delete requset of the user
+//so that when it is accessed sequentially , it encounters at the last
+
+
+app.get("/user", (req, res) => {
+  res.send({ name: "harsh", course: "btech" });
+});
+app.post("/user", (req, res) => {
+  res.send("data sent successfully to the db");
+});
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully!");
+});
+
+app.listen(1000, () =>
+  console.log("listening successfully on port number 1000 ")
+);
